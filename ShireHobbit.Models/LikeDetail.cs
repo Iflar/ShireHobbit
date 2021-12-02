3073ad86445fe90851995e4f4dbd72c7f60a5fa8
Danny Redden <dannyredden18@gmail.com>
2021-12-01 22:17:51 -0500

    like service updates

---
+++ ShireHobbit.Models/LikeDetail.cs
@@ -0,0 +1,17 @@
+﻿using ShireHobbit.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShireHobbit.Models
+{
+    public class LikeDetail
+    {
+        public int LikeId { get; set; }
+        public Guid OwnerId { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
